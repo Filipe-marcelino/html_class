f502a2441be122a515e4f7bcb6b2d767a49a3d10
Filipin <160949589+Filipe-marcelino@users.noreply.github.com>
2024-08-01 10:16:31 -0300

    Delete fazer_coisas directory

--- fazer_coisas/timer.py
+++
@@ -1,7 +0,0 @@
-import time as relogio
-print("Hello...")
-relogio.sleep(3)
-print("world!")
-
-
-print(relogio.time)
--- fazer_coisas/timer2.py
+++
@@ -1,6 +0,0 @@
-import time as relogio
-
-agr = relogio.gmtime()
-formatando = relogio.strftime('%DD%MM%AA',agr)
-
-print(formatando)
